d6c575022e8be9d72fd2a0024895b3dbed11c187
David Sanders <dsanderscanada@gmail.com>
2019-06-15 16:11:13 -0400

    Add noninteractive to k8s install

--- azure-k8s-bare/templates/k8s-scripts/install-k8s.sh
+++ azure-k8s-bare/templates/k8s-scripts/install-k8s.sh
@@ -4,7 +4,8 @@ sudo DEBIAN_FRONTEND=noninteractive \
         apt-get -o Dpkg::Options::="--force-confold" \
         -q \
         --yes \
-        kubeadm=1.14.1-00 \
-        kubelet=1.14.1-00 \
-        kubectl=1.14.1-00
+        install \
+          kubeadm=1.14.1-00 \
+          kubelet=1.14.1-00 \
+          kubectl=1.14.1-00
 sleep 2
