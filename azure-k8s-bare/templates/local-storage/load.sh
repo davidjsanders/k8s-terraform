a34a9ffbba4de0ede71fb490150794f3c9bfb921
David Sanders <dsanderscanada@gmail.com>
2019-07-16 19:13:24 -0400

    Fix local-storage and Nexus OSS

---
+++ azure-k8s-bare/templates/local-storage/load.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# -------------------------------------------------------------------
+#
+# Module:         k8s-terraform
+# Submodule:      templates/k8s-scripts/load-cowbull.sh
+# Environments:   all
+# Purpose:        Bash shell script to apply any yaml files found in
+#                 the cowbull sub-directory.
+#
+# Created on:     24 June 2019
+# Created by:     David Sanders
+# Creator email:  dsanderscanada@nospam-gmail.com
+#
+# -------------------------------------------------------------------
+# Modifed On   | Modified By                 | Release Notes
+# -------------------------------------------------------------------
+# 24 Jun 2019  | David Sanders               | First release.
+# -------------------------------------------------------------------
+# 26 Jun 2019  | David Sanders               | Add space between each
+#              |                             | yaml apply.
+# -------------------------------------------------------------------
+
+# Include the banner function for logging purposes (see 
+# templates/banner.sh)
+#
+source ~/scripts/banner.sh
+
+yaml_files=$(ls -1 /datadrive/azadmin/local-storage/[0-9]*.yaml)
+for file in $yaml_files
+do
+    echo "Applying yaml for: $file"
+    kubectl apply -f $file
+    echo
+done
+echo "Done."
+echo
