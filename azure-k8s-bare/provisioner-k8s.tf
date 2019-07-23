12b5cd4c1ad8689b67341fbf3d6e6aff0821dcfa
David Sanders <dsanderscanada@gmail.com>
2019-07-23 18:50:41 -0400

    Point provisioner at 50-ingress for Traefik

--- azure-k8s-bare/provisioner-k8s.tf
+++ azure-k8s-bare/provisioner-k8s.tf
@@ -95,7 +95,7 @@ resource "null_resource" "k8s" {
 
     provisioner "file" {
         content = "${data.template_file.ingress-yaml.rendered}"
-        destination = "/home/${var.vm-adminuser}/scripts/traefik/5-ingress.yaml"
+        destination = "/home/${var.vm-adminuser}/scripts/traefik/50-ingress.yaml"
     }
 
     provisioner "file" {
