9a03c967b1f5c46eda5ddac6e34f406a1f03c818
David Sanders <dsanderscanada@gmail.com>
2019-09-15 20:42:29 -0400

    Refactor playbook

---
+++ azure-k8s-ansible/data-playbook.tf
@@ -0,0 +1,34 @@
+# -------------------------------------------------------------------
+#
+# Module:         k8s-terraform/azure-k8s-ansible
+# Submodule:      data-ansible-inventory.tf
+# Environments:   all
+# Purpose:        Module to set the data in the Ansible playbook
+#                 for all variables.
+#
+# Created on:     15 September 2019
+# Created by:     David Sanders
+# Creator email:  dsanderscanada@nospam-gmail.com
+#
+# -------------------------------------------------------------------
+# Modifed On   | Modified By                 | Release Notes
+# -------------------------------------------------------------------
+# 15 Sep 2019  | David Sanders               | First release.
+# -------------------------------------------------------------------
+
+# Compute and interpolate the variables required for the hosts file
+data "template_file" "template-playbook-yaml" {
+  template = "${file("ansible/k8s-playbook/playbook.yml")}"
+
+  vars {
+    kubeadm_api = "kubeadm.k8s.io"
+    kubeadm_api_version = "v1beta1"
+    kubeadm_api_advertise_ip = "10.70.1.6"
+    kubeadm_cert_dir = "/etc/kubernetes/pki"
+    kubeadm_cluster_name = "kubernetes"
+    kubeadm_pod_subnet = "192.168.0.0/16"
+    kubeadm_service_subnet = "10.96.0.0/12"
+    kubeadm_k8s_version = "v1.14.3"
+  }
+}
+
